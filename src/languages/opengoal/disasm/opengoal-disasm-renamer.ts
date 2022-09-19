6049cee21fa3046aa89e950a5f3a32ae56cfb099
Tyler Wilding <xtvaser@gmail.com>
2022-09-19 16:18:54 -0400

    decomp: extract most of the logic from the renamer so it can be used elsewhere

--- src/languages/opengoal/disasm/opengoal-disasm-renamer.ts
+++ src/languages/opengoal/disasm/opengoal-disasm-renamer.ts
@@ -1,12 +1,10 @@
-import { stringify } from "comment-json";
-import { writeFileSync } from "fs";
-import { join } from "path";
 import * as vscode from "vscode";
+import { updateVarCasts } from "../../../decomp/utils";
+import { getSymbolAtPosition } from "../../common/utils";
 import {
-  getCastFileData,
-  getDecompilerConfigDirectory,
-} from "../../../utils/decomp-tools";
-import { getWorkspaceFolderByName } from "../../../utils/workspace";
+  determineCurrentFunctionName,
+  getSymbolsArgumentInfo,
+} from "../opengoal-tools";
 
 export class OpenGOALDisasmRenameProvider implements vscode.RenameProvider {
   public async provideRenameEdits(
@@ -15,162 +13,22 @@ export class OpenGOALDisasmRenameProvider implements vscode.RenameProvider {
     newName: string,
     token: vscode.CancellationToken
   ): Promise<vscode.WorkspaceEdit | undefined> {
-    const symbolRange = document.getWordRangeAtPosition(position, /[\w\-.]+/g);
-    if (symbolRange === undefined) {
+    const symbol = getSymbolAtPosition(document, position);
+    if (symbol === undefined) {
       return;
     }
-    const symbol = document.getText(symbolRange);
 
-    // TODO - likely doesn't work on states!
-
-    // TODO - detect if its method/function args in a better way thatn just if it's on
-    // the same line as a defun/defmethod
     const line = document.lineAt(position.line).text;
-    const isArgument = line.includes("defun") || line.includes("defmethod");
-    // If it's an argument, we have to figure out the index
-    let argumentIndex = undefined;
-    let argumentCount = undefined;
-    if (isArgument) {
-      const matches = [...line.matchAll(/(\([^\s(]*\s[^\s)]*\))/g)];
-      if (matches.length == 0) {
-        return;
-      }
-      let tempIdx = 0;
-      for (const match of matches[0]) {
-        const [argName, argType] = match
-          .toString()
-          .replace("(", "")
-          .replace(")", "")
-          .split(" ");
-        if (argName === symbol) {
-          argumentIndex = tempIdx;
-          argumentCount = matches.length;
-          break;
-        }
-        tempIdx++;
-      }
-      if (argumentIndex === undefined || argumentCount === undefined) {
-        return;
-      }
-    }
-
-    // Get the function this is related to (walk back until a defun/defmethod is found)
-    // TODO - probably could eventually replace this with an LSP integration eventually
-    // methods are annoying because in cast files they are referred to by the method id, not their name
-    let funcName = undefined;
-    for (let i = position.line; i > 0; i--) {
-      const currLine = document.lineAt(i).text;
-      const matches = [
-        ...currLine.matchAll(/(?:defun(?:-debug)?|defmethod)\s+([^\s]*)/g),
-      ];
-      if (matches.length == 1) {
-        // Functions are easy
-        if (currLine.includes("defun")) {
-          funcName = matches[0][1].toString();
-          break;
-        } else {
-          // methods are more difficult, for now we walk back until we find
-          // an empty line, then grab the info from the line after that
-          let prevLineIdx = i - 1;
-          while (
-            prevLineIdx > 0 &&
-            document.lineAt(prevLineIdx).text.trim() !== ""
-          ) {
-            prevLineIdx--;
-          }
-          const defmethodLine = document.lineAt(prevLineIdx + 1).text;
-          const methodMatches = [
-            ...defmethodLine.matchAll(
-              /;; definition for method (\d+) of type (.*)/g
-            ),
-          ];
-          if (methodMatches.length == 1) {
-            funcName = `(method ${methodMatches[0][1]} ${methodMatches[0][2]})`;
-          }
-          break;
-        }
-      }
-    }
+    const argMeta = getSymbolsArgumentInfo(line, symbol);
+    const funcName = determineCurrentFunctionName(document, position);
     if (funcName === undefined) {
       return;
     }
 
-    // Update the var-names file
-    const projectRoot = getWorkspaceFolderByName("jak-project");
-    if (projectRoot === undefined) {
-      vscode.window.showErrorMessage(
-        "OpenGOAL - Unable to locate 'jak-project' workspace folder"
-      );
-      return;
-    }
-
-    const varNameData = getCastFileData(
-      projectRoot,
-      document,
-      "var_names.jsonc"
-    );
-    if (varNameData === undefined) {
-      return;
-    }
-
-    if (!(funcName in varNameData)) {
-      varNameData[funcName] = {};
-    }
-    if (isArgument) {
-      if (argumentIndex === undefined || argumentCount === undefined) {
-        return;
-      }
-      if ("args" in varNameData[funcName]) {
-        // We assume that all slots are filled up already, as this is required
-        varNameData[funcName].args[argumentIndex] = newName;
-      } else {
-        // Otherwise, we initialize it properly
-        // TODO - supporting `null` here would be nice
-        varNameData[funcName].args = [];
-        for (let i = 0; i < argumentCount; i++) {
-          if (i == argumentIndex) {
-            varNameData[funcName].args[i] = newName;
-          } else {
-            varNameData[funcName].args[i] = `arg${i}`;
-          }
-        }
-      }
-    } else {
-      // if "vars" is in
-      if ("vars" in varNameData[funcName]) {
-        // Check to see if the current symbol has already been renamed
-        let internalVar = undefined;
-        for (const [key, value] of Object.entries(varNameData[funcName].vars)) {
-          if (value === symbol) {
-            internalVar = key;
-            break;
-          }
-        }
-        if (internalVar !== undefined) {
-          varNameData[funcName].vars[internalVar] = newName;
-        } else {
-          varNameData[funcName].vars[symbol] = newName;
-        }
-      } else {
-        varNameData[funcName]["vars"] = {};
-        varNameData[funcName]["vars"][symbol] = newName;
-      }
-    }
-
-    // Write out cast file change
-    const configDir = await getDecompilerConfigDirectory(document.uri);
-    if (configDir === undefined) {
-      return;
-    }
-    const filePath = join(configDir, "var_names.jsonc");
-
-    writeFileSync(filePath, stringify(varNameData, null, 2));
+    await updateVarCasts(document, funcName, argMeta, symbol, newName);
 
     // The actual renaming is done by the decompiler, which happens automatically if
     // auto decompilation is enabled
-    //
-    // TODO - maybe it should pause here and await the file to be re-decompiled so
-    // make it consistent (you don't do another rename) before values have been updated.
     return;
   }
 }
---
+++ src/languages/opengoal/opengoal-tools.ts
@@ -0,0 +1,104 @@
+import * as vscode from "vscode";
+
+export interface ArgumentMeta {
+  index: number;
+  totalCount: number;
+  isMethod: boolean;
+}
+
+// This function can only currently figure out arguments if they are on the same line as
+// a defun/defmethod/etc
+//
+// TODO - look ahead/behind from this line to see if we are in the arg list
+// TODO - likely doesn't work on states
+export function getSymbolsArgumentInfo(
+  line: string,
+  symbol: string
+): ArgumentMeta | undefined {
+  const isArgument = line.includes("defun") || line.includes("defmethod");
+  // TODO - 'new' method handling
+  const isMethod = line.includes("defmethod"); // if it's a method, make the first arg be `obj`
+  // If it's an argument, we have to figure out the index
+  let argumentIndex = undefined;
+  let argumentCount = undefined;
+  if (isArgument) {
+    const matches = [...line.matchAll(/(\([^\s(]*\s[^\s)]*\))/g)];
+    if (matches.length == 0) {
+      return undefined;
+    }
+    let tempIdx = 0;
+    for (const match of matches) {
+      const [argName, argType] = match[1]
+        .toString()
+        .replace("(", "")
+        .replace(")", "")
+        .split(" ");
+      if (argName === symbol) {
+        argumentIndex = tempIdx;
+        argumentCount = matches.length;
+        break;
+      }
+      tempIdx++;
+    }
+    if (argumentIndex === undefined || argumentCount === undefined) {
+      return undefined;
+    }
+  } else {
+    return undefined;
+  }
+  return {
+    index: argumentIndex,
+    totalCount: argumentCount,
+    isMethod: isMethod,
+  };
+}
+
+// Determines the name of the current function/method that we are in
+export function determineCurrentFunctionName(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): string | undefined {
+  for (let i = position.line; i > 0; i--) {
+    const currLine = document.lineAt(i).text;
+    const matches = [
+      ...currLine.matchAll(/(?:defun(?:-debug)?|defmethod)\s+([^\s]*)/g),
+    ];
+    if (matches.length == 1) {
+      // Functions are easy
+      if (currLine.includes("defun")) {
+        return matches[0][1].toString();
+      } else {
+        // methods are more difficult, for now we walk back until we run out
+        // of lines or hit an empty line
+        //
+        // hopefully we find a comment giving us the information we require
+        // before then
+        let prevLineIdx = i - 1;
+        while (
+          prevLineIdx > 0 &&
+          document.lineAt(prevLineIdx).text.trim() !== ""
+        ) {
+          if (
+            document
+              .lineAt(prevLineIdx)
+              .text.toLowerCase()
+              .includes(";; definition for method")
+          ) {
+            const methodMatches = [
+              ...document
+                .lineAt(prevLineIdx)
+                .text.matchAll(/;; definition for method (\d+) of type (.*)/g),
+            ];
+            if (methodMatches.length == 1) {
+              return `(method ${methodMatches[0][1]} ${methodMatches[0][2]})`;
+            } else {
+              return undefined;
+            }
+          }
+          prevLineIdx--;
+        }
+      }
+    }
+  }
+  return undefined;
+}
